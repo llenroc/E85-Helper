3e84fcfae3b57afc27788d9ff14ad893074523d0
Owen Johnson <owen@owenjohnson.info>
2014-08-19 23:46:30 -0500

    Basically Started Over. Forgot to Commit Little bits.

---
+++ Flex Fuel Calculator/Flex Fuel Calculator.Shared/StringFormatConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace Flex_Fuel_Calculator
+{
+    public class StringFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string formatString = parameter as string;
+            if (!string.IsNullOrEmpty(formatString))
+                return string.Format(formatString, value);
+
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
