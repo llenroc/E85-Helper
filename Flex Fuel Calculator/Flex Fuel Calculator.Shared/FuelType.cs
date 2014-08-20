3a8bf8a7edf056b2f16bbe28f5c177b0628b253a
Owen Johnson <owen@owenjohnson.info>
2014-08-20 00:41:11 -0500

    saving work before bed

---
+++ Flex Fuel Calculator/Flex Fuel Calculator.Shared/FuelType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flex_Fuel_Calculator
+{
+    public class FuelType
+    {
+        public string Name { get; set; }
+        public double PricePerVolumeUnit { get; set; }
+        public double GallonOfGasEquivalent { get; set; }
+        public double KgOfCarbonEmissionsPerVolumeUnit { get; set; }
+        public double VehicleSpecificDistanceUnitsPerVolumeUnit { get; set; }
+        public double PricePerDistanceUnit { get { return PricePerVolumeUnit / VehicleSpecificDistanceUnitsPerVolumeUnit; } }
+        public double KiloGramsOfCarbonEmissionsPerDistanceUnit { get { return KgOfCarbonEmissionsPerVolumeUnit * GallonOfGasEquivalent / VehicleSpecificDistanceUnitsPerVolumeUnit; } }
+    }
+}
---
+++ Flex Fuel Calculator/Flex Fuel Calculator.Shared/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Flex_Fuel_Calculator
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            Fuels = new ObservableCollection<FuelType>()
+            {
+                new FuelType{ Name = "Regular Gasoline - No Ethanol", GallonOfGasEquivalent = 1.0f, PricePerVolumeUnit = 3.75f, VehicleSpecificDistanceUnitsPerVolumeUnit = 35, KgOfCarbonEmissionsPerVolumeUnit = 8.887},
+                new FuelType{ Name = "Super Unleaded - 10% Ethanol", GallonOfGasEquivalent= .9814f, PricePerVolumeUnit= 3.65f, VehicleSpecificDistanceUnitsPerVolumeUnit= 33, KgOfCarbonEmissionsPerVolumeUnit = 8.887 *.90},
+                new FuelType{ Name = "E85 - 85% Ethanol", GallonOfGasEquivalent = .7194f, PricePerVolumeUnit= 2.64f, VehicleSpecificDistanceUnitsPerVolumeUnit= 23, KgOfCarbonEmissionsPerVolumeUnit = 8.887 * .15},
+                new FuelType{ Name = "Electricity - Coal", GallonOfGasEquivalent = 1/33.4, PricePerVolumeUnit= 3.34, KgOfCarbonEmissionsPerVolumeUnit = .95*33.4, VehicleSpecificDistanceUnitsPerVolumeUnit =98},
+                new FuelType{ Name = "Electricity - Natural Gas", GallonOfGasEquivalent = 1/33.4, PricePerVolumeUnit= 3.34, KgOfCarbonEmissionsPerVolumeUnit = .6*33.4, VehicleSpecificDistanceUnitsPerVolumeUnit = 98},
+                new FuelType{ Name = "Electricity - Oil", GallonOfGasEquivalent = 1/33.4, PricePerVolumeUnit= 3.34, KgOfCarbonEmissionsPerVolumeUnit = .9*33.4, VehicleSpecificDistanceUnitsPerVolumeUnit=98},
+                new FuelType{ Name = "Electricity - Wind,Solar,Etc.", GallonOfGasEquivalent = 1/33.4, PricePerVolumeUnit= 3.34, KgOfCarbonEmissionsPerVolumeUnit = 0, VehicleSpecificDistanceUnitsPerVolumeUnit=98},
+            };
+        }
+        public IList<FuelType> Fuels { get; set; }
+    }
+}
